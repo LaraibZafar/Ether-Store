a771337c7c9b258ee6499c826cf3108d3a633cc6
LaraibZafar <zafarhassan30@hotmail.com>
2020-06-11 20:57:45 +0500

    Styled the Home Page

--- src/components/menu-item.component/menu-item.component.jsx
+++ src/components/menu-item.component/menu-item.component.jsx
@@ -3,15 +3,16 @@ import "./menu-item.styles.scss";
 import { findAllByPlaceholderText } from "@testing-library/react";
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div
-    className={`${size} menu-item`}
-    style={{
-      backgroundImage: `url(${imageUrl})`,
-      //backgroundColor: "black",
-    }}
-  >
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-Image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+        //backgroundColor: "black",
+      }}
+    />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">Shop Now</span>
     </div>
   </div>
