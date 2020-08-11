ee946f1784833fb87664a3437aa95fdf7485589f
LaraibZafar <zafarhassan30@hotmail.com>
2020-08-11 15:22:51 +0500

    pretty much finished the entire front-end of the website

--- src/page-components/category-page-component/category-page.component.jsx
+++ src/page-components/category-page-component/category-page.component.jsx
@@ -4,11 +4,19 @@ import "./category-page.styles.scss";
 import { connect } from "react-redux";
 import { selectCategoryData } from "../../redux/shop-item/shop-item.selector";
 
+import ShopItem from "../../components/shop-item-component/shop-item.component";
+
 const CategoryPage = ({ match, categoryItems }) => {
+  const { title, items } = categoryItems;
+  console.log(items);
   return (
     <div className="category-page">
-      <h1>Category Page</h1>
-      {console.log(categoryItems)}
+      <h1 className="title">{title}</h1>
+      <div className="items">
+        {items.map(({ id, ...othervars }) => (
+          <ShopItem key={id} {...othervars} />
+        ))}
+      </div>
     </div>
   );
 };
