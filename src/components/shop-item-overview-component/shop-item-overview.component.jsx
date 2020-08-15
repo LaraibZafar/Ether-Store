6562200aa8cec587f6bd5394f8d84101ec9c707c
LaraibZafar <zafarhassan30@hotmail.com>
2020-08-15 18:19:21 +0500

    Made my selectors more efficient by converting Array Operations to Hash Operations, next milestone = add payment

--- src/components/shop-item-overview-component/shop-item-overview.component.jsx
+++ src/components/shop-item-overview-component/shop-item-overview.component.jsx
@@ -5,21 +5,24 @@ import ShopItemList from "../shop-item-list-component/shop-item-list.component";
 
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectShopData } from "../../redux/shop-item/shop-item.selector";
+import { selectShopDataArray } from "../../redux/shop-item/shop-item.selector";
 
-const ShopItemPreview = ({ collections }) => (
-  <div className="itemPreview">
-    {collections.map(({ id, title, items }) => (
-      <div key={id}>
-        <h1>{title}</h1>
-        <ShopItemList id={id} items={items} />
-      </div>
-    ))}
-  </div>
-);
+const ShopItemPreview = ({ collections }) => {
+  console.log(collections);
+  return (
+    <div className="itemPreview">
+      {collections.map(({ id, title, items }) => (
+        <div key={id}>
+          <h1>{title}</h1>
+          <ShopItemList id={id} items={items} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectShopData,
+  collections: selectShopDataArray,
 });
 
 export default connect(mapStateToProps)(ShopItemPreview);
--- src/redux/shop-item/shop-item.selector.js
+++ src/redux/shop-item/shop-item.selector.js
@@ -1,14 +1,6 @@
 import { createSelector } from "reselect";
 import shopItemReducer from "./shop-item.reducer";
 
-const ITEM_TO_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShopItem = (state) => state.shop;
 
 export const selectShopData = createSelector(
@@ -16,7 +8,10 @@ export const selectShopData = createSelector(
   (shop) => shop.shopItems
 );
 
+export const selectShopDataArray = createSelector(
+  [selectShopData],
+  (shopItems) => Object.keys(shopItems).map((itemKeys) => shopItems[itemKeys])
+);
+
 export const selectCategoryData = (categoryItem) =>
-  createSelector([selectShopData], (shopItems) =>
-    shopItems.find((shopItem) => shopItem.id === ITEM_TO_ID_MAP[categoryItem])
-  );
+  createSelector([selectShopData], (shopItems) => shopItems[categoryItem]);
